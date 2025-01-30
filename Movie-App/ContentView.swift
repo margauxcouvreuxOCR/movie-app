aba69b8f95bad5d521852d25dbda7164d72c2ff7
margaux_mazaleyras <margaux.cvrx@gmail.com>
2025-01-30 15:02:49 +0100

    Commit for primary connexion

---
+++ Movie-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie-App
+//
+//  Created by Margaux Mazaleyras on 30/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie-App/Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_AppApp.swift
+//  Movie-App
+//
+//  Created by Margaux Mazaleyras on 30/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
